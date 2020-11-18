0f4d43d17f5a7580b63da49b46eb69a4a3ea15ad
cjconnor18 <cjallen1988@gmail.com>
2020-11-18 12:35:25 -0600

    first commit

---
+++ CovidWorldOMeterScraper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CovidWorldOMeterScraper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
