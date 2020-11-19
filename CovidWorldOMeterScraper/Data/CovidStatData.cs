b6b7e369b4886a13c30d2d8f44af044840147a1e
cjconnor18 <cjallen1988@gmail.com>
2020-11-19 09:24:01 -0600

    updated Methods in CovidStatData

--- CovidWorldOMeterScraper/Data/CovidStatData.cs
+++ CovidWorldOMeterScraper/Data/CovidStatData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CovidWorldOMeterScraper.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,49 @@ namespace CovidWorldOMeterScraper.Data
 {
     public class CovidStatData
     {
+        private static Dictionary<int, CovidStat> CovidStats = new Dictionary<int, CovidStat>();
+        private static Boolean IsDataLoaded = false;
+
+        private static bool LoadData()
+        {
+            if(!IsDataLoaded)
+            {
+                IsDataLoaded = true;
+                return true;
+            }
+            return true;
+        }
+
+        //create
+        public static bool Add(CovidStat covidStat)
+        {
+            CovidStats.Add(covidStat.StatId, covidStat);
+            return true;
+        }
+
+        //read
+        public static IEnumerable<CovidStat> GetAll()
+        {
+            LoadData();
+            return CovidStats.Values;
+        }
+
+        public static CovidStat GetById(int id)
+        {
+            LoadData();
+            return CovidStats[id];
+        }
+
+        //delete
+        public static bool Remove(int id)
+        {
+            if(CovidStats.ContainsKey(id))
+            {
+                CovidStats.Remove(id);
+                return true;
+            }
+            return false;
+        }
+        
     }
 }
